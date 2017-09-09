357861bb70a83d6aa883486101c7ea9e6c7a049c
huaxi <huaxi@DESKTOP-DGDN6E0>
2017-09-09 13:44:45 +0800

    完善模型旋转，缩放，移动处理

---
+++ ly-editor/src/name/huliqing/editor/converter/field/ScaleConverter.java
@@ -0,0 +1,335 @@
+/*
+ * LuoYing is a program used to make 3D RPG game.
+ * Copyright (c) 2014-2016 Huliqing <31703299@qq.com>
+ * 
+ * This file is part of LuoYing.
+ *
+ * LuoYing is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LuoYing is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with LuoYing.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package name.huliqing.editor.converter.field;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.jme3.math.FastMath;
+import com.jme3.math.Quaternion;
+import com.jme3.math.Vector2f;
+import com.jme3.math.Vector3f;
+import com.jme3.scene.Spatial;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import name.huliqing.editor.constants.DataFormatConstants;
+import name.huliqing.editor.constants.StyleConstants;
+import name.huliqing.editor.converter.SimpleFieldConverter;
+import name.huliqing.editor.edit.scene.JfxSceneEdit;
+import name.huliqing.luoying.data.EntityData;
+import name.huliqing.luoying.manager.ModelManager;
+import name.huliqing.luoying.object.entity.Entity;
+import name.huliqing.luoying.xml.Converter;
+
+/**
+ *
+ * @author huliqing
+ */
+public class ScaleConverter extends SimpleFieldConverter{
+    
+    private final VBox layout = new VBox();
+    private final HBox sizeLayout = new HBox();
+    private final HBox controlLayout = new HBox();
+    
+    private final HBox xLayout = new HBox();
+    private final Label xLabel = new Label("长");
+    private final TextField xField = new TextField("");
+    
+    private final HBox yLayout = new HBox();
+    private final Label yLabel = new Label("高");
+    private final TextField yField = new TextField("");
+    
+    private final HBox zLayout = new HBox();
+    private final Label zLabel = new Label("宽");
+    private final TextField zField = new TextField("");
+    
+    private String lastX = "";
+    private String lastY = "";
+    private String lastZ = "";
+    private Vector3f lastValue;
+    
+    private final CheckBox sameScale = new CheckBox("等比缩放");
+    
+    private final ChangeListener<Boolean> focusedListener = (ObservableValue<? extends Boolean> observable
+            , Boolean oldValue, Boolean newValue) -> {
+        if (newValue) {
+            return;
+        }
+        updateAndSave();
+    };
+    private final EventHandler<KeyEvent> keyHandler = new EventHandler<KeyEvent>() {
+        @Override
+        public void handle(KeyEvent event) {
+            if (event.getCode() == KeyCode.ENTER) {
+                updateAndSave();
+            }
+        }
+    };
+    
+    public ScaleConverter() {
+        layout.getStyleClass().add(StyleConstants.CLASS_HVBOX);
+        /*xLayout.getStyleClass().add(StyleConstants.CLASS_HVBOX);
+        yLayout.getStyleClass().add(StyleConstants.CLASS_HVBOX);
+        zLayout.getStyleClass().add(StyleConstants.CLASS_HVBOX);
+        
+        layout.getChildren().addAll(xLayout, yLayout, zLayout);
+        xLayout.getChildren().addAll(xLabel, xField);
+        yLayout.getChildren().addAll(yLabel, yField);
+        zLayout.getChildren().addAll(zLabel, zField);
+        
+        xLayout.setAlignment(Pos.CENTER_LEFT);
+        yLayout.setAlignment(Pos.CENTER_LEFT);
+        zLayout.setAlignment(Pos.CENTER_LEFT);
+        */
+        xField.focusedProperty().addListener(focusedListener);
+        yField.focusedProperty().addListener(focusedListener);
+        zField.focusedProperty().addListener(focusedListener);
+        xField.setOnKeyPressed(keyHandler);
+        yField.setOnKeyPressed(keyHandler);
+        zField.setOnKeyPressed(keyHandler);
+        
+        xLabel.setMinWidth(25);
+        yLabel.setMinWidth(25);
+        zLabel.setMinWidth(25);
+        
+        sizeLayout.getChildren().addAll(xLabel, xField, yLabel, yField, zLabel, zField);
+        controlLayout.getChildren().add(sameScale);
+        
+        layout.getChildren().addAll(sizeLayout, controlLayout);
+        
+        sizeLayout.setPadding(new Insets(10, 10, 10, 10));
+        controlLayout.setPadding(new Insets(10, 10, 10, 20));
+        layout.setPadding(new Insets(10, 10, 10, 10));
+        
+        setLayoutStyle();
+    }
+    
+    private void setLayoutStyle() {
+    	xLabel.setStyle("-fx-padding: 4;");
+		xField.setStyle("-fx-border-color: blue;-fx-border-width: 1px;-fx-background-radius: 5;");
+		xField.setPromptText("长度(厘米)");
+		xField.setPrefWidth(90);
+		
+		yLabel.setStyle("-fx-padding: 4;");
+		yField.setPromptText("宽度(厘米)");
+		yField.setStyle("-fx-border-color: blue;-fx-border-width: 1px;-fx-background-radius: 5;");
+		yField.setPrefWidth(90);
+		
+		zLabel.setStyle("-fx-padding: 4;");
+		zField.setStyle("-fx-border-color: blue;-fx-border-width: 1px;-fx-background-radius: 5;");
+		zField.setPromptText("高度(厘米)");
+		zField.setPrefWidth(90);
+		
+		sameScale.setSelected(true);
+    }
+
+    @Override
+    protected Node createLayout() {
+        return layout;
+    }
+    
+    private void updateAndSave() {
+        boolean changed = false;
+        // 缩放比例
+        float ratioX = 1;
+        float ratioY = 1;
+        float ratioZ = 1;
+        
+        float oldX = Float.parseFloat(lastX);
+        float oldY = Float.parseFloat(lastY);
+        float oldZ = Float.parseFloat(lastZ);
+        
+        if (!xField.getText().equals(lastX)) {
+        	ratioX = Float.parseFloat(xField.getText()) / Float.parseFloat(lastX);
+            lastX = xField.getText();
+            changed = true;
+        }
+        if (!yField.getText().equals(lastY)) {
+        	ratioY = Float.parseFloat(yField.getText()) / Float.parseFloat(lastY);
+            lastY = yField.getText();
+            changed = true;
+        }
+        if (!zField.getText().equals(lastZ)) {
+        	ratioZ = Float.parseFloat(zField.getText()) / Float.parseFloat(lastZ);
+            lastZ = zField.getText();
+            changed = true;
+        }
+        
+        // 是否等比例缩放，计算缩放尺寸
+        if (sameScale.isSelected()) {
+        	if (ratioX != 1) {
+        		ratioY = ratioX;
+        		ratioZ = ratioX;
+        		ratioX = 1;
+        	} else if (ratioY != 1) {
+        		ratioX = ratioY;
+        		ratioZ = ratioY;
+        		ratioY = 1;
+        	} else if (ratioZ != 1) {
+        		ratioY = ratioZ;
+        		ratioX = ratioZ;
+        		ratioZ = 1;
+        	}
+        	lastX = DataFormatConstants.DECIMAL_FORMAT_2.format(ratioX * Float.parseFloat(lastX));
+        	lastY = DataFormatConstants.DECIMAL_FORMAT_2.format(ratioY * Float.parseFloat(lastY));
+        	lastZ = DataFormatConstants.DECIMAL_FORMAT_2.format(ratioZ * Float.parseFloat(lastZ));
+        	xField.setText(String.valueOf(lastX));
+        	yField.setText(String.valueOf(lastY));
+        	zField.setText(String.valueOf(lastZ));
+        }
+        
+        if (changed) {
+            
+            try {
+                Vector3f newVec = new Vector3f(Float.parseFloat(lastX), Float.parseFloat(lastY),Float.parseFloat(lastZ));
+                Map<String, Object> updateAttrs = new HashMap<>();
+                Vector3f modelSize = getModelSize();
+                // 读取的是厘米，转换成比例
+                updateAttrs.put(field, newVec.divide(modelSize));
+
+                // 模型中心位置
+                Vector3f modelCenter = getModelCenter();
+                
+                // 模型的旋转角度
+                Vector3f modelRotation = getModelRotation();
+                
+                // 保持模型位置
+            	Vector3f location = getEntityLocation();
+            	
+            	// X,Z轴跟据比例计算缩放后的新的中心位置
+            	float newX = modelCenter.getX() * (newVec.getX() / oldX - 1);
+            	// 保持Y轴
+            	//float newY = location.getY();
+            	float newZ = modelCenter.getZ() * (newVec.getZ() / oldZ - 1);
+            	
+            	// 水平旋转角度，以模型为中心旋转模型角度
+            	Vector2f rotate2 = new Vector2f(newX, newZ);
+            	rotate2.rotateAroundOrigin(modelRotation.getY(), true);
+
+            	// 水平偏移量
+            	Vector3f globalLoc3 = new Vector3f(rotate2.getX(), 1, rotate2.getY());
+            	
+            	// 通过方位和水平位移移动3D模型
+            	Vector3f newLocation = location.subtract(globalLoc3);
+        
+            	updateAttrs.put("location", newLocation);
+                super.updateMultAttibutes(updateAttrs);
+                
+                addUndoRedo(lastValue, new Vector3f(newVec));
+                lastValue = new Vector3f(newVec);
+            } catch (NumberFormatException e) {
+                // 如果全为空则清除该参数
+                if (lastX.equals("") && lastY.equals("") && lastZ.equals("")) {
+                    updateAttribute(null);
+                    addUndoRedo(lastValue, null);
+                    lastValue = null;
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void updateUI() {
+        Object propertyValue = data.getAttribute(field);
+        // 存储的是比例，转换成尺寸厘米
+        lastValue = Converter.getAsVector3f(propertyValue).mult(getModelSize());
+        
+        if (lastValue != null) {
+            xField.setText(DataFormatConstants.DECIMAL_FORMAT_2.format(lastValue.x) + "");
+            yField.setText(DataFormatConstants.DECIMAL_FORMAT_2.format(lastValue.y) + "");
+            zField.setText(DataFormatConstants.DECIMAL_FORMAT_2.format(lastValue.z) + "");
+        } else {
+            xField.setText("");
+            yField.setText("");
+            zField.setText("");
+        }
+        lastX = xField.getText();
+        lastY = yField.getText();
+        lastZ = zField.getText();
+    }
+    
+    /**
+     * 获得当前模型的尺寸
+     * @return
+     */
+    private  Vector3f getModelSize() {
+    	List<Entity> entities = ((JfxSceneEdit)jfxEdit).getJmeEdit().getScene().getEntities();
+    	 Vector3f modelSize = null;
+    	for (Entity et : entities) {
+    		if (et.getEntityId() == data.getUniqueId()) {
+    			Spatial sp = et.getSpatial();
+    			modelSize = ModelManager.getInstance().getSize(sp);
+    			return modelSize;
+    		}
+    	}
+    	
+    	return null;
+    }
+    
+    /**
+     * 获得模型的中心位置
+     * @return
+     */
+    private Vector3f getModelCenter() {
+    	List<Entity> entities = ((JfxSceneEdit)jfxEdit).getJmeEdit().getScene().getEntities();
+	   	Vector3f modelCenter = null;
+	   	for (Entity et : entities) {
+	   		if (et.getEntityId() == data.getUniqueId()) {
+	   			Spatial sp = et.getSpatial();
+	   			modelCenter = ModelManager.getInstance().getModelCenter(sp);
+	   			return modelCenter;
+	   		}
+	   	}
+	   	
+	   	return null;
+    }
+    
+    /**
+     * 获得模型的旋转角度
+     * @return
+     */
+    private Vector3f getModelRotation() {
+    	Quaternion quaternion = ((EntityData)data).getRotation();
+    	 float[] temp = new float[3];
+    	quaternion.toAngles(temp);
+    	
+    	return new Vector3f(temp[0], temp[1], temp[2]);
+    }
+    
+    /**
+     * 获得实体位置信息
+     * @return
+     */
+    private Vector3f getEntityLocation() {
+    	return ((EntityData)data).getLocation();
+    }
+}
---
+++ ly-editor/src/name/huliqing/editor/converter/field/SceneLightEntitiesConverter.java
@@ -0,0 +1,388 @@
+/*
+ * LuoYing is a program used to make 3D RPG game.
+ * Copyright (c) 2014-2016 Huliqing <31703299@qq.com>
+ * 
+ * This file is part of LuoYing.
+ *
+ * LuoYing is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LuoYing is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with LuoYing.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package name.huliqing.editor.converter.field;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javafx.beans.value.ObservableValue;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToolBar;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.util.Callback;
+import name.huliqing.editor.constants.AssetConstants;
+import name.huliqing.editor.constants.EntityConstants;
+import name.huliqing.editor.constants.StyleConstants;
+import name.huliqing.editor.converter.DataConverter;
+import name.huliqing.editor.converter.FieldConverter;
+import name.huliqing.editor.edit.Mode;
+import name.huliqing.editor.edit.UndoRedo;
+import name.huliqing.editor.edit.controls.ControlTile;
+import name.huliqing.editor.edit.controls.entity.EntityControlTile;
+import name.huliqing.editor.edit.scene.JfxSceneEdit;
+import name.huliqing.editor.edit.scene.JfxSceneEditListener;
+import name.huliqing.editor.edit.scene.SceneEdit;
+import name.huliqing.editor.manager.ConverterManager;
+import name.huliqing.editor.ui.utils.JfxUtils;
+import name.huliqing.fxswing.Jfx;
+import name.huliqing.luoying.data.EntityData;
+import name.huliqing.luoying.object.entity.Entity;
+import name.huliqing.luoying.object.scene.Scene;
+
+/**
+ * 将光设置用列表展示
+ * @author huliqing
+ */
+public class SceneLightEntitiesConverter extends FieldConverter<JfxSceneEdit, EntityData> implements JfxSceneEditListener {
+
+    private final VBox layout = new VBox();
+    private final ToolBar toolBar = new ToolBar();
+    
+    private final FilterListView filterListView = new FilterListView();
+    private boolean ignoreSelectEvent;
+    
+    private final Map<EntityData, DataConverter> entityConverterMaps = new HashMap();
+    // 当前正在显示的EntityConverter
+    private DataConverter dataConverter;
+
+    /**
+     * 光源：太阳光和灯光
+     */
+    private static List<String> entityTypes = new ArrayList<>(Arrays.asList(EntityConstants.ENTITY_DIRECTIONAL_LIGHT, EntityConstants.ENTITY_POINT_LIGHT));
+    
+    public SceneLightEntitiesConverter() {
+        // 工具栏
+        Button add = new Button("", JfxUtils.createIcon(AssetConstants.INTERFACE_ICON_ADD));
+        Button remove = new Button("", JfxUtils.createIcon(AssetConstants.INTERFACE_ICON_SUBTRACT));
+        ToggleButton multSelect = new ToggleButton("", JfxUtils.createIcon(AssetConstants.INTERFACE_ICON_MULT_SELECT));
+
+        remove.setOnAction(e -> {
+            List<EntityData> eds = filterListView.listView.getSelectionModel().getSelectedItems();
+            if (eds == null || eds.isEmpty())
+                return;
+            
+            Jfx.runOnJme(() -> {
+                List<EntityControlTile> ectsRemove = new ArrayList(eds.size());
+                SceneEdit se = jfxEdit.getJmeEdit();
+                for (EntityData ed : eds) {
+                    EntityControlTile ect = se.getEntityControlTile(ed);
+                    if (ect != null) {
+                        ectsRemove.add(ect);
+                    }
+                }
+                EntityRemovedUndoRedo erur = new EntityRemovedUndoRedo(ectsRemove);
+                erur.redo();
+                se.addUndoRedo(erur);
+                
+                // 这一步不需要，因为已经做了监听场景实体移除的功能,当场景实体移除的时候会自动触发onEntityRemoved方法
+//                Jfx.runOnJfx(() -> {
+//                    listView.getItems().removeAll(eds);
+//                });
+            });
+        });
+        multSelect.selectedProperty().addListener((ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) -> {
+            if (newValue) {
+                filterListView.listView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+            } else {
+                filterListView.listView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+            }
+        });
+        
+        // 列表
+        filterListView.listView.setCellFactory(new CellInner());
+        filterListView.listView.getSelectionModel().selectedItemProperty().addListener(this::onJfxSelectChanged);
+        
+        layout.getStyleClass().add(StyleConstants.CLASS_HVBOX);
+        toolBar.getItems().addAll(add, remove, multSelect);
+        layout.getChildren().addAll(toolBar, filterListView);
+    }
+        
+    @Override
+    protected Node createLayout() {
+        return layout;
+    }
+    
+    @Override
+    public void notifyChanged() {
+        filterListView.listView.refresh();// 这一句允许刷新列表中的物体名称。
+        super.notifyChanged();
+    }
+    
+    @Override
+    public void initialize() {
+        super.initialize();
+        // 用于监听3D场景中选择物体的变化
+        jfxEdit.addListener(this);
+    }
+    
+    @Override
+    public void cleanup() {
+        if (dataConverter != null) {
+            if (dataConverter.isInitialized()) {
+                dataConverter.cleanup();
+            }
+            dataConverter = null;
+        }
+        jfxEdit.removeListener(this);
+        super.cleanup(); 
+    }
+
+    private void onJfxSelectChanged(ObservableValue observable, EntityData oldValue, EntityData newValue) {
+        if (ignoreSelectEvent) {
+            return;
+        }
+        if (newValue != null) {
+            // 注：重新设置选择的时候会触发事件，回调到onSelectChanged(EntitySelectObj)
+            // 要注意避免在该方法中导致死循环重复。
+            jfxEdit.setSelected(newValue);
+            doUpdateEntityView(newValue);
+        }
+    }
+
+    @Override
+    public void onModeChanged(Mode mode) {
+        // 不管
+    }
+
+    @Override
+    public void onEntityAdded(EntityData entityData) {
+    	if (entityTypes.contains(entityData.getTagName())) {
+    		filterListView.listView.getItems().add(entityData);
+    	}
+    }
+
+    @Override
+    public void onEntityRemoved(EntityData ed) {
+        filterListView.listView.getItems().remove(ed);
+        entityConverterMaps.remove(ed);
+    }
+
+    @Override
+    public void onSelectChanged(ControlTile selectObj) {
+        if (selectObj == null) {
+            ignoreSelectEvent = true;
+            filterListView.listView.getSelectionModel().clearSelection();
+            doUpdateEntityView(null);
+            ignoreSelectEvent = false;
+            return;
+        }
+        if (!(selectObj instanceof EntityControlTile))
+            return;
+        
+        ignoreSelectEvent = true;
+        EntityData ed = ((EntityControlTile)selectObj).getTarget().getData();
+        filterListView.listView.getSelectionModel().select(ed);
+        doUpdateEntityView(ed);
+        ignoreSelectEvent = false;
+    }
+    
+    private void doUpdateEntityView(EntityData entityData) {
+        if (entityData == null) {
+            return;
+        }
+        
+        DataConverter dc = entityConverterMaps.get(entityData);
+        if (dc == null) {
+            dc = ConverterManager.createDataConverter(jfxEdit, entityData, this);
+            entityConverterMaps.put(entityData, dc);
+        }
+        if (dataConverter != null) {
+            dataConverter.cleanup();
+        }
+        dataConverter = dc;
+        dataConverter.initialize();
+        getParent().setChildLayout(entityData.getId(), dataConverter);
+    }
+
+    @Override
+    public void updateView() {
+        // ignore
+    }
+
+    private class CellInner implements Callback<ListView<EntityData>, ListCell<EntityData>> {
+
+        @Override
+        public ListCell<EntityData> call(ListView<EntityData> param) {
+            ListCell<EntityData> lc = new ListCell<EntityData>() {
+                @Override
+                protected void updateItem(EntityData item, boolean empty) {
+                    super.updateItem(item, empty);
+                    setText(null);
+                    setGraphic(null);
+                    if (!empty && item != null) {
+                        setGraphic(new ListCellRow(item));
+                    }
+                    
+                }
+            };
+            return lc;
+        }
+    }
+    
+    private class ListCellRow extends GridPane {
+        private final Label nameLabel = new Label("");
+        private final ToggleButton enableBtn = new ToggleButton("", JfxUtils.createIcon(AssetConstants.INTERFACE_ICON_EYE));
+        private final EntityData entityData;
+        public ListCellRow(EntityData item) {
+            entityData = item;
+            String nameStr = item.getId();
+            if (item.getName() != null && !item.getName().isEmpty()) {
+                nameStr += "(" + item.getName() + ")";
+            }
+            nameLabel.setText(nameStr);
+            nameLabel.setAlignment(Pos.CENTER_LEFT);
+            enableBtn.setSelected(item.getAsBoolean("enabled", true));
+            enableBtn.setAlignment(Pos.CENTER_RIGHT);
+            enableBtn.setPrefWidth(16);
+            enableBtn.selectedProperty().addListener((ObservableValue<? extends Boolean> ob, Boolean oldValue, Boolean newValue) -> {
+                Scene scene = jfxEdit.getJmeEdit().getScene();
+                if (scene == null)
+                    return;
+                Jfx.runOnJme(() -> {
+                    Entity entity = scene.getEntity(entityData.getUniqueId());
+                    if (entity != null) {
+                        entity.setEnabled(newValue);
+                    }
+                });
+            });
+            setAlignment(Pos.CENTER_LEFT);
+            addRow(0, nameLabel, enableBtn);
+            GridPane.setHgrow(nameLabel, Priority.ALWAYS);
+            GridPane.setHgrow(enableBtn, Priority.NEVER);
+        }
+    }
+    
+    private class EntityRemovedUndoRedo implements UndoRedo {
+        private final List<EntityControlTile> ectRemoved = new ArrayList();
+        public EntityRemovedUndoRedo(List<EntityControlTile> ectRemoved) {
+            this.ectRemoved.addAll(ectRemoved);
+        }
+        @Override
+        public void undo() {
+            SceneEdit se = jfxEdit.getJmeEdit();
+            for (int i = ectRemoved.size() - 1; i >= 0; i--) {
+                se.addControlTile(ectRemoved.get(i));
+            }
+        }
+        @Override
+        public void redo() {
+            SceneEdit se = jfxEdit.getJmeEdit();
+            for (int i = 0; i < ectRemoved.size(); i++) {
+                se.removeControlTile(ectRemoved.get(i));
+            }
+        }
+    }
+    
+    // 场景实体列表,包含过滤功能
+    private class FilterListView extends VBox {
+        
+        private final StackPane filterPane = new StackPane();
+        private final HBox imageView = new HBox(JfxUtils.createIcon(AssetConstants.INTERFACE_ICON_SEARCH));
+        private final TextField inputFilter = new TextField();
+        
+        private final ListView<EntityData> listView = new ListView();
+        private final List<EntityData> tempList = new ArrayList();
+        
+        public FilterListView() {
+            imageView.setPadding(new Insets(0, 0, 0, 10));
+            imageView.setMinWidth(16);
+            imageView.setMaxWidth(16);
+            imageView.prefHeightProperty().bind(filterPane.heightProperty());
+            imageView.setAlignment(Pos.CENTER);
+            inputFilter.prefWidthProperty().bind(filterPane.widthProperty());
+            inputFilter.prefHeightProperty().bind(filterPane.heightProperty());
+            inputFilter.setPadding(new Insets(0, 0, 0, 25));
+            filterPane.setMinHeight(25);
+            filterPane.setAlignment(Pos.CENTER_LEFT);
+            
+            filterPane.getChildren().add(inputFilter);
+            filterPane.getChildren().add(imageView);
+            getChildren().add(filterPane);
+            getChildren().add(listView);
+            
+            inputFilter.textProperty().addListener((ObservableValue<? extends String> observable, String oldValue, String newValue) -> {
+                updateList();
+            });
+            
+            listView.setStyle(StyleConstants.CSS_CORNER_ROUND_BOTTOM);
+            inputFilter.setStyle(StyleConstants.CSS_CORNER_ROUND_TOP);
+        }
+        
+        public void updateList() {
+            Scene scene = jfxEdit.getJmeEdit().getScene();
+            if (scene == null) {
+                listView.getItems().clear();
+                tempList.clear();
+                return;
+            }
+            tempList.clear();
+            listView.getItems().clear();
+            
+            // 2017/08/17 显示模型
+            List<Entity> entities = scene.getEntities(entityTypes);
+            String filterText = inputFilter.getText().trim().toLowerCase();
+            // 无任何过滤
+            if (filterText.isEmpty()) {
+                for (Entity e : entities) {
+                    tempList.add(e.getData());
+                }
+                listView.getItems().addAll(tempList);
+                return;
+            }
+            
+            // 允许通过半角","号分隔多个过滤项
+            String[] filters = filterText.split(",");
+            String entityStr;
+            for (Entity e : entities) {
+                if (filters.length <= 0) {
+                    tempList.add(e.getData());
+                    continue;
+                }
+                entityStr = e.getData().getId();
+                if (e.getData().getName() != null) {
+                    entityStr += "(" + e.getData().getName() + ")";
+                }
+                for (String filter : filters) {
+                    if (!filter.isEmpty() && entityStr.toLowerCase().contains(filter)) {
+                        tempList.add(e.getData());
+                    }
+                }
+            }
+            listView.getItems().addAll(tempList);
+        }
+        
+    }
+}
